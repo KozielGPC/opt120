fa4ce511a336633a1cebf8786f254b6c0aff97ee
Márcio Gabriel <gpcgabriel0@gmail.com>
2024-03-29 13:37:30 -0300

    feat: add create atividade modal

--- app/lib/atividades_screen.dart
+++ app/lib/atividades_screen.dart
@@ -4,11 +4,15 @@ import 'package:app/atividades_service.dart'; // Import the AtividadeService or 
 
 class AtividadesScreen extends StatefulWidget {
   @override
-  _AtividadeScreenState createState() => _AtividadeScreenState();
+  _AtividadesScreenState createState() => _AtividadesScreenState();
 }
 
-class _AtividadeScreenState extends State<AtividadesScreen> {
+class _AtividadesScreenState extends State<AtividadesScreen> {
   List<List<String>> _atividadeTableData = []; // Data for the table
+
+  final TextEditingController _tituloController = TextEditingController();
+  final TextEditingController _notaController = TextEditingController();
+  final TextEditingController _descricaoController = TextEditingController();
 
   @override
   void initState() {
@@ -53,14 +57,16 @@ class _AtividadeScreenState extends State<AtividadesScreen> {
                       ),
                       ElevatedButton(
                         onPressed: () {
-                          // Handle button press to create new atividade
+                          _showCreateAtividadeModal(
+                              context); // Show modal to create new atividade
                         },
                         child: Text('Create Atividade'),
                       ),
                     ],
                   ),
                 ),
-                DataTableWidget( // Display the table of atividades
+                DataTableWidget(
+                  // Display the table of atividades
                   fieldNames: ['Titulo', 'Nota', 'Descrição'],
                   inputData: _atividadeTableData,
                 ),
@@ -70,5 +76,73 @@ class _AtividadeScreenState extends State<AtividadesScreen> {
         ),
       ),
     );
+  }
+
+  void _showCreateAtividadeModal(BuildContext context) {
+    final TextEditingController tituloController = TextEditingController();
+    final TextEditingController notaController = TextEditingController();
+    final TextEditingController descricaoController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Create Atividade'),
+          content: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                  controller: tituloController,
+                  decoration: InputDecoration(labelText: 'Titulo'),
+                ),
+                TextFormField(
+                  controller: notaController,
+                  decoration: InputDecoration(labelText: 'Nota'),
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                ),
+                TextFormField(
+                  controller: descricaoController,
+                  decoration: InputDecoration(labelText: 'Descrição'),
+                  maxLines: null,
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _createAtividade(context);
+              },
+              child: Text('Create'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _createAtividade(BuildContext context) async {
+    final String titulo = _tituloController.text;
+    final String nota = _notaController.text;
+    final String descricao = _descricaoController.text;
+
+    try {
+      await AtividadeService.createAtividade(titulo, nota, descricao);
+      await _loadAtividades();
+      _tituloController.clear();
+      _notaController.clear();
+      _descricaoController.clear();
+      Navigator.of(context).pop(); // Close the modal
+    } catch (e) {
+      // Handle error
+      print('Error creating atividade: $e');
+    }
   }
 }
