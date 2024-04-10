ebf00cca657c9481810eb0589bf38f5648e20d38
Márcio Gabriel <gpcgabriel0@gmail.com>
2024-04-10 11:12:06 -0300

    feat: add new user routes

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const { Router } = express;
+const { userController } = require("../controllers/user");
 
 const userRoutes = Router();
 
-userRoutes.get("/", (req, res) => {
-    res.send("Hello World!");
+userRoutes.get("/", userController.findMany);
+
+userRoutes.get("/:id", (req, res) => {
+    res.send("Get User by id");
+});
+
+userRoutes.post("/", (req, res) => {
+    res.send("Post Users");
+});
+
+userRoutes.patch("/:id", (req, res) => {
+    res.send("Patch Users");
+});
+
+userRoutes.delete("/:id", (req, res) => {
+    res.send("Delete Users");
 });
 
 module.exports = {
