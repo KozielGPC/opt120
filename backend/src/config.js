b7d0a5d2706e286a870302cab1b4bfee71d55e55
Márcio Gabriel <gpcgabriel0@gmail.com>
2024-04-15 23:22:36 -0300

    feat: add mysql env connections

--- backend/src/config.js
+++ backend/src/config.js
@@ -3,7 +3,11 @@ dotenv.config();
 
 const config = {
   PORT: process.env.PORT,
-  DATABASE_URL: process.env.DATABASE_URL,
+  DB_NAME: process.env.DB_NAME,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
 };
 
 Object.entries(config).forEach(([key, value]) => {
--- backend/src/providers/database.js
+++ backend/src/providers/database.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql');
+const { config } = require("../config");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root"
+  host: config.DB_HOST,
+  port: config.DB_PORT,
+  user: config.DB_USER,
+  password: config.DB_PASSWORD,
+  database: config.DB_NAME
 });
 
 module.exports = {
