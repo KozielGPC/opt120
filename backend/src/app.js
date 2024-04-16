466024119fd314114ac5b56ad59ca2cd8c541121
Márcio Gabriel <gpcgabriel0@gmail.com>
2024-04-15 23:22:20 -0300

    feat: add db connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { activityRoutes } = require("./routes/activity");
 const { userRoutes } = require("./routes/user");
+const { db } = require("./providers/database");
 
 const app = express();
 
@@ -11,6 +12,24 @@ app.use(express.json());
 app.use("/activity", activityRoutes);
 app.use("/user", userRoutes);
 
+db.connect(function (err) {
+  if (err) {
+    console.log("Failed to connect to database!");
+    throw err;
+  }
+  console.log("Connected!");
+
+  db.query("create database if not exists opt120", function (err, result) {
+    if (err) throw err;
+    console.log("Creating opt120");
+  });
+
+  db.query("use opt120", function (err, result) {
+    if (err) throw err;
+    console.log("Using opt120");
+  });
+});
+
 module.exports = {
-    app
+  app,
 };
